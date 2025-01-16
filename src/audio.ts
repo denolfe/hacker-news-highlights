eada78aec2ae24ae3183dced7d2f58885da2f5cf
Elliot DeNolf <denolfe@gmail.com>
2025-01-15 23:28:06 -0500

    feat: join audio files

--- src/audio.ts
+++ src/audio.ts
@@ -1,70 +1,34 @@
 import fs from 'fs/promises'
-import path from 'path'
 import OpenAI from 'openai'
+import path from 'path'
 
 import { createHash } from 'crypto'
-import { directoryOrFileExists } from './utils/directoryOrFileExists'
 import { DATA_DIR } from './constants'
 
-export async function generateAudioFromText(summaries: string[]): Promise<string> {
+export async function generateAudioFromText(summaries: string[]): Promise<string[]> {
   console.log('Generating audio...')
   const hash = createHash('sha256').update(summaries.join('\n\n')).digest('hex')
-  const outputFileWithPrefix = path.resolve(DATA_DIR, `./podcast-${hash}`)
-
-  const fileExists = await directoryOrFileExists(outputFileWithPrefix + '-0.mp3')
-  if (fileExists) {
-    console.log(`Existing audio file matches hash: ${hash}. Skipping audio generation.`)
-    return outputFileWithPrefix
-  }
+  const filePrefix = `podcast-${hash}`
 
   const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
   })
 
-  const splitSummaries = await intelligentSplit(summaries)
+  const audioFilenames: string[] = []
 
-  for (const [i, summaryPart] of splitSummaries.entries()) {
+  for (const [i, summary] of summaries.entries()) {
     const mp3 = await openai.audio.speech.create({
       model: 'tts-1',
       voice: 'shimmer',
-      input: summaryPart.join('\n\n'),
+      input: summary,
     })
 
     const buffer = Buffer.from(await mp3.arrayBuffer())
-    const filename = `${outputFileWithPrefix}-${i}.mp3`
+    const filename = `${filePrefix}-${i}.mp3`
     console.log(`Writing audio to file to ${filename}`)
-    await fs.writeFile(filename, buffer)
+    await fs.writeFile(path.resolve(DATA_DIR, filename), buffer)
+    audioFilenames.push(filename)
   }
 
-  return outputFileWithPrefix
-}
-
-async function intelligentSplit(summaries: string[]): Promise<Array<string[]>> {
-  const maxCharacters = 4096
-  const result: Array<string[]> = []
-  let currentSummaries = summaries
-
-  while (currentSummaries.length > 0) {
-    let characterCount = 0
-    let splitIndex = currentSummaries.length
-
-    for (let i = 0; i < currentSummaries.length; i++) {
-      if (characterCount + currentSummaries[i].length > maxCharacters) {
-        splitIndex = i
-        break
-      }
-      characterCount += currentSummaries[i].length
-    }
-
-    result.push(currentSummaries.slice(0, splitIndex))
-    currentSummaries = currentSummaries.slice(splitIndex)
-
-    console.log(`Split at index ${splitIndex}. Remaining summaries: ${currentSummaries.length}`)
-  }
-
-  result.map((part, i) => {
-    console.log(`Part ${i + 1}: ${part.join('\n').length} characters`)
-  })
-
-  return result
+  return audioFilenames
 }
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { summarize } from './ai'
 import { fetchTopStories } from './hn'
 import { generateAudioFromText } from './audio'
 import { createDataDir } from './utils/createDataDir'
+import { joinAudioFiles } from './utils/joinAudioFiles'
+import { DATA_DIR } from './constants'
 
 loadEnvFile(path.resolve(__dirname, '../.env'))
 
@@ -11,7 +13,8 @@ async function main() {
   await createDataDir()
   const storyData = await fetchTopStories(10)
   const summaries = await summarize(storyData)
-  await generateAudioFromText(summaries)
+  const audioFilenames = await generateAudioFromText(summaries)
+  await joinAudioFiles(audioFilenames, path.resolve(DATA_DIR, 'output.mp3'))
   console.log('Done!')
 }
 
