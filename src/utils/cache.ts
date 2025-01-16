d50bb8f1322f51b2b46ba7fd2b1315dfca3d4020
Elliot DeNolf <denolfe@gmail.com>
2025-01-16 00:37:03 -0500

    feat: rework cache

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -3,48 +3,29 @@ import path from 'path'
 import { directoryOrFileExists } from './directoryOrFileExists'
 
 const CACHE_DIR = path.resolve(__dirname, '../../cache')
+const debug = process.env.DEBUG === 'true'
 
-// File cache to write text to '../../cache' directory
-export class Cache {
-  private static instance: Cache
-  private readonly cacheDir: string
-  debug: boolean
-
-  private constructor() {
-    this.cacheDir = CACHE_DIR
-    this.debug = process.env.DEBUG === 'true'
+export async function initCacheDir() {
+  console.log(`[CACHE] Initializing cache directory: ${CACHE_DIR}`)
+  if (!(await directoryOrFileExists(CACHE_DIR))) {
+    await fs.mkdir(CACHE_DIR)
   }
+}
 
-  static async getInstance(): Promise<Cache> {
-    if (!Cache.instance) {
-      Cache.instance = new Cache()
-      await Cache.initCacheDir()
-    }
-    return Cache.instance
+export async function writeToCache(key: string, data: string) {
+  if (debug) {
+    console.log(`[CACHE] Writing to cache: ${key}`)
   }
+  await fs.writeFile(path.resolve(CACHE_DIR, key), data)
+}
 
-  private static async initCacheDir() {
-    console.log(`[CACHE] Initializing cache directory: ${CACHE_DIR}`)
-    if (!(await directoryOrFileExists(CACHE_DIR))) {
-      await fs.mkdir(CACHE_DIR)
-    }
+export async function readFromCache(key: string) {
+  const location = path.resolve(CACHE_DIR, key)
+  if (!(await directoryOrFileExists(location))) {
+    return null
   }
-
-  async write(key: string, data: string) {
-    if (this.debug) {
-      console.log(`[CACHE] Writing to cache: ${key}`)
-    }
-    await fs.writeFile(path.resolve(this.cacheDir, key), data)
+  if (debug) {
+    console.log(`[CACHE] Reading from cache: ${key}`)
   }
-
-  async read(key: string) {
-    const location = path.resolve(this.cacheDir, key)
-    if (!(await directoryOrFileExists(location))) {
-      return null
-    }
-    if (this.debug) {
-      console.log(`[CACHE] Reading from cache: ${key}`)
-    }
-    return await fs.readFile(location, 'utf-8')
-  }
+  return await fs.readFile(location, 'utf-8')
 }
