08c7d008a911cf2273cf17a2f67a120c2b4b897e
Elliot DeNolf <denolfe@gmail.com>
2025-01-18 01:34:41 -0500

    feat: child loggers

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -10,11 +10,21 @@ export const log = {
     process.stderr.write(`WARNING: ${format(...args)}\n`)
   },
   info: (...args: unknown[]) => {
-    process.stderr.write(`INFO: ${format(...args)}\n`)
+    process.stderr.write(`${format(...args)}\n`)
   },
   error: (...args: unknown[]) => {
     process.stderr.write(`ERROR: ${format(...args)}\n`)
   },
+}
+
+export const childLogger = (name: string) => {
+  const prefix = `[${name}]`
+  return {
+    debug: (...args: unknown[]) => log.debug(prefix, ...args),
+    warning: (...args: unknown[]) => log.warning(prefix, ...args),
+    info: (...args: unknown[]) => log.info(prefix, ...args),
+    error: (...args: unknown[]) => log.error(prefix, ...args),
+  }
 }
 
 export function isDebug() {
