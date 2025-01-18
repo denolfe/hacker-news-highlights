6e655fab5e17aafdd6ab940f0c952cdb70f165bb
Elliot DeNolf <denolfe@gmail.com>
2025-01-18 13:35:06 -0500

    feat: improve show notes

--- src/lib/show-notes.ts
+++ src/lib/show-notes.ts
@@ -10,10 +10,16 @@ export async function generateShowNotes({
   stories: StoryDataAggregate[]
   introText: string
 }) {
-  let showNotes = stories
-    .map(story => {
-      return `\n${story.title}\nLink: ${story.url}\nComments: ${story.hnUrl}\n`
-    })
+  const formattedDate = new Date().toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+    timeZone: 'America/New_York',
+  })
+
+  let showNotes = `This is a recap of the top 10 posts on Hacker News on ${formattedDate}.\n\n`
+  showNotes += stories
+    .map(story => `${story.title}\nLink: ${story.url}\nHN: ${story.hnUrl}\n`)
     .join('\n')
 
   showNotes += `\n\nTranscript:\n${introText}\n${stories.map(s => s.summary).join('\n\n')}`
