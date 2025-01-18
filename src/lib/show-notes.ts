082f46aadc284388e47d124180024d5e09cc0032
Elliot DeNolf <denolfe@gmail.com>
2025-01-18 01:27:34 -0500

    feat: adjust show notes

--- src/lib/show-notes.ts
+++ src/lib/show-notes.ts
@@ -12,11 +12,11 @@ export async function generateShowNotes({
 }) {
   let showNotes = stories
     .map(story => {
-      return `\n## ${story.title}\n[Original Link](${story.url})\n`
+      return `\n${story.title}\nLink: ${story.url})\n`
     })
     .join('\n')
 
-  showNotes += `\n\n## Transcript\n${introText}\n${stories.map(s => s.summary).join('\n')}`
+  showNotes += `\n\nTranscript:\n${introText}\n${stories.map(s => s.summary).join('\n\n')}`
 
   await writeToFile(path.resolve(CACHE_DIR, 'show-notes.txt'), showNotes)
 }
