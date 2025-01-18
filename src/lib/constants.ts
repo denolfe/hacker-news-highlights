8ac23ad9c8986bb82c25c1b0261ea936feb852ed
Elliot DeNolf <denolfe@gmail.com>
2025-01-17 23:50:57 -0500

    feat: random imperative in intro

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,3 +3,13 @@ import path from 'path'
 export const CACHE_DIR = path.resolve(__dirname, '../../cache')
 
 export const podcastOutro = `Thank you for joining us on another episode of The Hacker News Recap. Have a great day!`
+export const IMPERATIVE_PHRASES = [
+  'begin',
+  'dive in',
+  'dive right in',
+  'get going',
+  'get into it',
+  'get started',
+  'jump in',
+  'jump right in',
+]
