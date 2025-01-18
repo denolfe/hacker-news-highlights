2dc5670fd0114ffd7dd657ea996bcb7c69d07565
Elliot DeNolf <denolfe@gmail.com>
2025-01-18 14:54:26 -0500

    feat: cache covered stories

--- src/lib/hn.ts
+++ src/lib/hn.ts
@@ -10,8 +10,10 @@ const logger = childLogger('HN')
 
 export async function fetchTopStories(count: number = 10): Promise<StoryOutput[]> {
   logger.info(`Fetching top ${count} stories...`)
+
+  // Fetch additional stories to account for stories covered in previous episodes
   const response = await fetch(
-    `https://hn.algolia.com/api/v1/search?tags=front_page&hitsPerPage=${count}`,
+    `https://hn.algolia.com/api/v1/search?tags=front_page&hitsPerPage=${count + 10}`,
   )
   const data = (await response.json()) as ResponseData
 
@@ -22,17 +24,48 @@ export async function fetchTopStories(count: number = 10): Promise<StoryOutput[]
     storyId: s.story_id,
   }))
 
+  // Check if we have already covered the story
+  const covered = await readFromCache('covered-stories')
+  const coveredStories: number[] = covered ? JSON.parse(covered) : []
+
+  logger.info(`Found ${coveredStories.length} covered stories`, {
+    coveredStories,
+  })
+
+  // Filter out stories we have already covered
+  const filtered = slim
+    .filter(s => {
+      const wasCovered = coveredStories.includes(s.storyId)
+      logger.debug({ storyId: s.storyId, wasCovered })
+      return !wasCovered
+    })
+    .slice(0, count)
+
+  logger.debug({ filtered })
+
+  if (filtered.length < count) {
+    const msg = `Not enough stories to cover. Found ${filtered.length}, expected ${count}`
+    logger.error(msg)
+    throw new Error(msg)
+  }
+
+  const newCovered = [...coveredStories, ...filtered.map(s => s.storyId)]
+  logger.debug({ newCovered })
+
+  // Save the covered stories
+  await writeToCache('covered-stories', JSON.stringify(newCovered))
+
   // Fetch the content and comments for each story
   const output: StoryOutput[] = []
-  for (const hit of slim) {
-    logger.info(`Fetching [${hit.storyId}] - ${hit.title} - ${hit.url}`)
-    const cacheKey = 'story-' + hit.storyId.toString()
+  for (const story of filtered) {
+    logger.info(`Fetching [${story.storyId}] - ${story.title} - ${story.url}`)
+    const cacheKey = 'story-' + story.storyId.toString()
 
     let htmlString = await readFromCache(cacheKey)
     if (!htmlString) {
-      htmlString = await fetch(hit.url).then(res => res.text())
+      htmlString = await fetch(story.url).then(res => res.text())
       if (!htmlString) {
-        logger.info(`No content found for ${hit.url}`)
+        logger.info(`No content found for ${story.url}`)
         continue
       }
       await writeToCache(cacheKey, htmlString)
@@ -54,16 +87,16 @@ export async function fetchTopStories(count: number = 10): Promise<StoryOutput[]
 
     logger.debug({ byline, excerpt, siteName })
 
-    const comments = await fetchStoryDataById(hit.storyId)
+    const comments = await fetchStoryDataById(story.storyId)
     output.push({
       content: textContent,
       comments,
-      title: hit.title,
-      url: hit.url,
-      storyId: hit.storyId,
+      title: story.title,
+      url: story.url,
+      storyId: story.storyId,
       // strip http(s), parse just the domain from the url
-      source: siteName ?? byline ?? new URL(hit.url).hostname.replace('www.', ''),
-      hnUrl: `https://news.ycombinator.com/item?id=${hit.storyId}`,
+      source: siteName ?? byline ?? new URL(story.url).hostname.replace('www.', ''),
+      hnUrl: `https://news.ycombinator.com/item?id=${story.storyId}`,
     })
   }
 
