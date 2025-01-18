8341f213acbf1c48c10f916732d0809ee20029a2
Elliot DeNolf <denolfe@gmail.com>
2025-01-18 01:27:19 -0500

    feat: use output dir

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,19 @@ import path from 'path'
 import { loadEnvFile } from 'process'
 import { generatePodcastIntro, summarize } from './lib/ai'
 import { fetchTopStories } from './lib/hn'
-import { generateAudioFromText } from './lib/audio'
-import { joinAudioFiles } from './utils/joinAudioFiles'
-import { CACHE_DIR, podcastOutro as outro } from './lib/constants'
+import { generateAudioFromText, joinAudioFiles } from './lib/audio'
+import { CACHE_DIR, OUTPUT_DIR, podcastOutro as outro } from './lib/constants'
 import { initCacheDir } from './utils/cache'
 import { log } from './utils/log'
 import { generateShowNotes } from './lib/show-notes'
+import { initOutputDir } from './utils/initOutputDir'
 
 loadEnvFile(path.resolve(__dirname, '../.env'))
 
 const args = process.argv.slice(2)
 
 async function main() {
+  await initOutputDir()
   await initCacheDir()
   const storyData = await fetchTopStories(args[0] ? parseInt(args[0]) : 10)
   const intro = await generatePodcastIntro(storyData)
@@ -23,7 +24,7 @@ async function main() {
     ...summaries,
     { summary: outro, storyId: 'outro' },
   ])
-  await joinAudioFiles(audioFilenames, path.resolve(CACHE_DIR, 'output.mp3'))
+  await joinAudioFiles(audioFilenames, path.resolve(OUTPUT_DIR, 'output.mp3'))
   await generateShowNotes({ stories: summaries, introText: intro.text })
   log.info('Done!')
 }
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,7 @@
 import path from 'path'
 
 export const CACHE_DIR = path.resolve(__dirname, '../../cache')
+export const OUTPUT_DIR = path.resolve(__dirname, '../../output')
 
 export const podcastOutro = `Thank you for joining us on another episode of The Hacker News Recap. Have a great day!`
 export const IMPERATIVE_PHRASES = [
