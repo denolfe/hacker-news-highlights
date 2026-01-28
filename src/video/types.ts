a3109f7201f9fcad6d0de2721a0d0f53e1701860
Elliot DeNolf <denolfe@gmail.com>
2026-01-27 23:41:06 -0500

    chore: add video types

---
+++ src/video/types.ts
@@ -0,0 +1,22 @@
+export type VideoChapter = {
+  title: string
+  source: string
+  screenshotPath: string
+  startFrame: number
+  durationFrames: number
+}
+
+export type VideoProps = {
+  chapters: VideoChapter[]
+  fps: number
+  totalDurationInFrames: number
+}
+
+export type ChapterInput = {
+  title: string
+  source: string
+  url: null | string
+  storyId: string
+  start: number
+  end: number
+}
